545eff6006b640804802dc4ee87a559b7b032fc9
Chethan30 <chetan611611611@gmail.com>
2022-03-24 17:14:18 +0530

    Setting Up React Router, NavigationBar, and Token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
-import LandingPage from "./components/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
+import { LoginContext } from "./context/LoginContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import CVScorer from "./pages/CVScorer/CVScorer";
+import Applications from "./pages/ApplicationsPage/Applications";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
+  // const [showProfile, setShowProfile] = useState(false);
+  const [username, setUsername] = useState("");
+  const [token, setToken] = useState("");
+
   return (
-    <div className="main">
-      <LandingPage />
-    </div>
+    <Router>
+      <LoginContext.Provider value={{ username, setUsername, setToken }}>
+        {token ? <HomePage /> : <LoginPage />}
+      </LoginContext.Provider>
+      <Routes>
+        {/* <Route path="home" element={<HomePage />} /> */}
+        <Route path="cvscorer" element={<CVScorer />} />
+        <Route path="applications" element={<Applications />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
