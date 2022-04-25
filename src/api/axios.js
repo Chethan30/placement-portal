be1fdc099b26e15855caef58d35843066adaeb56
GarvitaJ <60030070+GarvitaJ@users.noreply.github.com>
2022-04-26 00:54:50 +0530

    Auth header not here

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,14 +4,12 @@ import axios from 'axios';
 //creating n instance of axios by default
 // baseURL -> URL of backend server
 
-const token = 'Bearer '+ sessionStorage.getItem('token')
 
 const axiosClient = axios.create({
   baseURL: `http://localhost:5000/api/`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': token
   }
 });
 
