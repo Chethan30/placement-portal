83a781e90aed2ebd8b07fb90335c3b6831259cff
Chethan30 <chetan611611611@gmail.com>
2022-05-02 15:51:47 +0530

    SearchBar and UI Changes

--- src/components/CardLayout/JobCard.js
+++ src/components/CardLayout/JobCard.js
@@ -6,17 +6,19 @@ import JobType from "../../assets/JobTime.png";
 function JobCard(props) {
   const companyName = props.companyName;
   const jobRole = props.jobRole;
-  const jobID = props.jobID;
   const jobType = props.jobType;
-  const timeLeft = props.timeLeft;
-  const endDate = props.endDate;
+
+  let todayDate = new Date();
+  let endDate = new Date(props.endDate);
+  let difference = endDate.getTime() - todayDate.getTime();
+  let days = difference / (1000 * 3600 * 24);
+  const timeLeft = Math.round(days);
 
   return (
     <div className={styles.card}>
       <div className={styles.uppercard}>
         <span className={styles.companyname}> {companyName} </span>
         <div className={styles.timer}>{timeLeft} days left</div>
-        {/* Insert Timer Component here */}
       </div>
       <div className={styles.lowercard}>
         <div>
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "../../components/UI/Wrapper";
 import ProfileImage from "./ProfileImage";
+import { getProfileDetails } from "./apihandler";
 import GeneralDetails from "./Details/GeneralDetails";
 import EducationDetails from "./Details/EducationDetails";
+import LoadingPage from "../../components/LoadingPage/LoadingPage";
 import classes from "./Profile.module.css";
 
 function Profile() {
+  const [details, setDetails] = useState([]);
+  const [isProfileLoading, setProfileLoading] = useState(false);
+
+  let profileContent = (
+    <div className={classes.personal}>
+      <ProfileImage />
+      <GeneralDetails GenDets={details} />
+      <div />
+      <div />
+    </div>
+  );
+
+  let profileContent2 = (
+    <div className={classes.educational}>
+      <EducationDetails EduDets={details} />
+    </div>
+  );
+
+  const getDetailsHandler = async () => {
+    try {
+      const response = await getProfileDetails();
+      if (response.status === 200) {
+        setProfileLoading(false);
+        setDetails(response.data);
+        return response;
+      } else throw new Error("Error in loading profile!");
+    } catch (error) {
+      console.log("", error);
+    }
+  };
+
+  useEffect(() => {
+    setProfileLoading(true);
+    getDetailsHandler();
+  }, []);
+
   return (
     <Wrapper>
-      <div className={classes.personal}>
-        <ProfileImage />
-        <GeneralDetails />
-        <div />
-        <div />
-      </div>
-      <div className={classes.educational}>
-        <EducationDetails />
-      </div>
+      {isProfileLoading && <LoadingPage loadMessage="Getting Profile..." />}
+      {!isProfileLoading && profileContent}
+      {!isProfileLoading && profileContent2}
     </Wrapper>
   );
 }
