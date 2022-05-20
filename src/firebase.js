7f253dd905d4c7509b2c3b3e033b1ebf2305f141
GarvitaJ <60030070+GarvitaJ@users.noreply.github.com>
2022-05-20 11:20:22 +0530

    Add env variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHI50b3GKadA4kZlrqEb27o8lbGwuLY9U",
-  authDomain: "placement-portal-dsce2022.firebaseapp.com",
-  projectId: "placement-portal-dsce2022",
-  storageBucket: "placement-portal-dsce2022.appspot.com",
-  messagingSenderId: "844457063494",
-  appId: "1:844457063494:web:0207681a21ce6a249a8647"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
