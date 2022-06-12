9f2e61a83b7cc33b28981e869a924cfed5019425
Chethan30 <chetan611611611@gmail.com>
2022-06-12 17:35:54 +0530

    Deployement v2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,16 +1,14 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 //creating n instance of axios by default
 // baseURL -> URL of backend server
 
-
 const axiosClient = axios.create({
-  baseURL: `http://localhost:5000/api/`,
+  baseURL: `https://placement-portal-flask.herokuapp.com/api/`,
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  }
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosClient;
