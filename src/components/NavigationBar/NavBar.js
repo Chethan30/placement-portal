fd759b80c81f8713622bb34c38610341b25be5fb
Chethan30 <chetan611611611@gmail.com>
2022-06-17 13:14:36 +0530

    Fully Responsive Navbar for All Screen Sizes

--- src/components/NavigationBar/NavBar.js
+++ src/components/NavigationBar/NavBar.js
@@ -1,37 +1,65 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth-context";
 import styles from "./NavBar.module.css";
 function NavBar(props) {
   const { username } = useContext(AuthContext);
+  const [showLinks, setShowLinks] = useState(false);
+
+  const showMenuHandler = () => {
+    if (window.innerWidth < 768) {
+      setShowLinks(!showLinks);
+    } else {
+      return;
+    }
+  };
 
   return (
     <nav>
       <Link to="home" className={styles.home}>
         <li className={styles["logo-nav"]}>Placement Portal</li>
       </Link>
-      <ul className={styles.menu}>
-        <Link to="statistics" className={styles.cvscorer}>
+      <ul className={styles.menu} id={showLinks ? `${styles.show}` : ""}>
+        <Link
+          to="statistics"
+          className={styles.cvscorer}
+          onClick={showMenuHandler}
+        >
           <li>Stats</li>
         </Link>
-        <Link to="applications" className={styles.applications}>
+        <Link
+          to="applications"
+          className={styles.applications}
+          onClick={showMenuHandler}
+        >
           <li>Applications</li>
         </Link>
-        <Link to="forum" className={styles.notifications}>
+        <Link
+          to="forum"
+          className={styles.notifications}
+          onClick={showMenuHandler}
+        >
           <li>Forum</li>
         </Link>
-        <Link to="profile" className={styles.profile}>
+        <Link to="profile" className={styles.profile} onClick={showMenuHandler}>
           <li>
             <div className={styles.profileIcon}>
               {username ? username[0].toUpperCase() : null}
             </div>
           </li>
         </Link>
+        <button onClick={props.onLogout} className={styles.logout}>
+          Logout
+        </button>
       </ul>
-      <button onClick={props.onLogout} className={styles.logout}>
-        Logout
-      </button>
-      <button className={`${styles.hamburger}`}>
+      <button
+        className={
+          showLinks
+            ? `${styles.hamburger} ${styles["is-active"]}`
+            : `${styles.hamburger}`
+        }
+        onClick={showMenuHandler}
+      >
         <span></span>
         <span></span>
         <span></span>
